a4c2c6429ffa99e9e97fd4a3394e0b0dd1577628
Talha Younas <talhamughal52352@gmail.com>
2024-07-13 13:28:40 +0500

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -16,7 +16,8 @@ const User = require("../schemas/userSchema");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../uploads"));
+    console.log(path.join(__dirname, "../uploads/"));
+    cb(null, path.join(__dirname, "../uploads/"));
   },
   filename: (req, file, cb) => {
     cb(null, `${uuidv4()}.${file.originalname}`);
