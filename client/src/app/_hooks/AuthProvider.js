f842b458e62e09ad1506cf992d0b5c0499076911
Talha Younas <talhamughal52352@gmail.com>
2024-07-13 12:30:19 +0500

    Update AuthProvider.js

--- client/src/app/_hooks/AuthProvider.js
+++ client/src/app/_hooks/AuthProvider.js
@@ -6,7 +6,9 @@ const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(
-    localStorage.getItem("mernTestToken") || ""
+    typeof window !== "undefined"
+      ? localStorage.getItem("mernTestToken") || ""
+      : ""
   );
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
